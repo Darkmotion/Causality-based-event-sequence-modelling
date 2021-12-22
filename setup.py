32fb37060dd16e4636868cc353b942a38ff80b59
fjosp <fjosp.kfjelp@gmail.com>
2021-12-22 23:59:39 +0300

    Create a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Causality-based-event-sequence-modelling',
+    version='',
+    packages=['causal_nh', 'causal_nh.model', 'causal_nh.granger_causality'],
+    url='',
+    license='',
+    author='Skoltech_MSD',
+    author_email='polina.pilyugina@skoltech.ru',
+    description='Causality-based Neural Hawkes model'
+)
