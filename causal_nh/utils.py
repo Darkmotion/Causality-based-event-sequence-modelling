9b6e646938735eb0f87d7e6c3f147eecdf1e1084
Darkmotion <Darkmotion@mail.ru>
2021-12-24 02:38:11 +0300

    universal implementation

--- causal_nh/utils.py
+++ causal_nh/utils.py
@@ -11,39 +11,15 @@ def open_pkl_file(path, description):
         data = pickle.load(f, encoding="latin1")
         type_size = data["dim_process"]
         data = data[description]
+
     time_durations = []
     type_seqs = []
     seq_lens = []
+
     for i in range(len(data)):
-        event_seqs = np.array(
-            [
-                int(event["type_event"])
-                for event in data[i]
-                if int(event["type_event"]) <= 1
-            ]
-        )
-        event_seqs = (event_seqs - 1) * (-1)
-        type_seqs.append(torch.LongTensor(event_seqs))
-        time_durations.append(
-            torch.FloatTensor(
-                [
-                    float(event["time_since_last_event"])
-                    for event in data[i]
-                    if int(event["type_event"]) <= 1
-                ]
-            )
-        )
-        seq_lens.append(
-            len(
-                torch.LongTensor(
-                    [
-                        int(event["type_event"])
-                        for event in data[i]
-                        if int(event["type_event"]) <= 1
-                    ]
-                )
-            )
-        )
+        seq_lens.append(len(data[i]))
+        type_seqs.append(torch.LongTensor([int(event['type_event']) for event in data[i]]))
+        time_durations.append(torch.FloatTensor([float(event['time_since_last_event']) for event in data[i]]))
     return time_durations, type_seqs, seq_lens, type_size
 
 def open_pkl_file_original(path, description):
