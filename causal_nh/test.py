7980c40255c07d8edefd1dccbaf1a2d2429840eb
Darkmotion <Darkmotion@mail.ru>
2021-12-23 23:32:03 +0300

    universal implementation

--- causal_nh/test.py
+++ causal_nh/test.py
@@ -7,12 +7,25 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+from causal_nh.model.NeuralHawkesCuda import NeuralHawkes
+from sklearn.metrics import accuracy_score
 
 def simulate_prediction_original_nh(time_durations,
                                     type_tests,
                                     n_samples,
                                     model):
-    max_duration = torch.max(time_durations)
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print("You are using GPU acceleration.")
+        print("Number of GPU: ", torch.get_num_threads())
+    else:
+        device = torch.device("cpu")
+        print("CUDA is not Available. You are using CPU only.")
+        print("Number of cores: ", os.cpu_count())
+
+    model.to(device)
+    max_duration = torch.max(time_durations).to(device)
+
     simulated_duration = torch.sort(torch.empty(n_samples).uniform_(0, 40 * max_duration.item()))[0].reshape(
         n_samples, 1)
     time_durations = time_durations.expand(n_samples, time_durations.shape[-1])
@@ -27,34 +40,43 @@ def simulate_prediction_original_nh(time_durations,
     h_last, c_last, c_bar_last, decay_last, o_last = h_out[-2][0], c_out[-2][0], c_bar_out[-2][0], decay_out[-2][0], \
                                                      gate_out[-2][0]
     #########################
-    estimated_lambda_sum = torch.sum(F.softplus(model.hidden_lambda(simulated_h)), dim=-1)
+
+    if isinstance(model, NeuralHawkes):
+        estimated_lambda_sum = torch.sum(F.softplus(model.hidden_lambda(simulated_h)), dim=-1)
+    else:
+        estimated_lambda_sum = torch.sum(F.softplus(torch.matmul(model.hidden_lambda(simulated_h), model.weighted_A.t())), dim=-1)
     estimated_lambda_sum = estimated_lambda_sum.reshape(n_samples)
-    simulated_duration = simulated_duration.reshape(n_samples)
+    simulated_duration = simulated_duration.reshape(n_samples).to(device)
     simulated_integral_exp_terms = torch.stack(
         [(torch.sum(estimated_lambda_sum[:(i + 1)]) * (simulated_duration[i] / (i + 1))) for i in
          range(0, n_samples)])
     simulated_density = estimated_lambda_sum * torch.exp(-simulated_integral_exp_terms)
+    simulated_density.to(device)
     estimated_time = torch.sum(simulated_duration * simulated_density) * (40 * max_duration.item()) / n_samples
 
     # calculate intensity and types
+    type_tests = type_tests.to(device)
     type_input = model.emb(type_tests[0][-1])
     cell_i, cell_bar_updated, gate_decay, gate_output = model.lstm_cell(type_input, h_last, c_last, c_bar_last)
     _, hidden = model.lstm_cell.decay(cell_i, cell_bar_updated, gate_decay, gate_output, estimated_time)
 
     #########################
-    lambda_all = F.softplus(model.hidden_lambda(hidden))
-
+    if isinstance(model, NeuralHawkes):
+        lambda_all = F.softplus(model.hidden_lambda(hidden))
+    else:
+        lambda_all = F.softplus(torch.matmul(model.hidden_lambda(hidden), model.weighted_A.t()))
     _, estimated_type = torch.max(lambda_all, dim=-1)
     lambda_sum = torch.sum(lambda_all, dim=-1)
     return estimated_time, estimated_type, lambda_sum
 
 
-def test_prediction(test_data, idx_test_series, n_samples, start_test_idx, path_to_initialization, dataset_name):
+def test_prediction(test_data, idx_test_series, n_samples, start_test_idx, path_to_save, path_to_model, dataset_name):
     # read the model and simulate durations for integral
-    path_to_save = path_to_initialization + '_test_prediction_original_NH_'
-    path_to_model = path_to_initialization + '_model.pt'
     dim_process = test_data[3]
     test_data = test_data[:3]
+
+    model_name = path_to_model.split('/')[-1].split('.')[0]
+    print(model_name)
 
     time_durations, type_tests, seq_lens_lists = test_data
     time_duration, type_test = padding_full(time_durations, type_tests, seq_lens_lists, dim_process)
@@ -83,6 +105,7 @@ def test_prediction(test_data, idx_test_series, n_samples, start_test_idx, path_
         print("prediction at event {0} on a sequence of length {1} is done".format(idx, max_len))
 
     rmse = sqrt(mean_squared_error(original_time, estimated_times))
+
     figure, ax = plt.subplots(nrows=1, ncols=3, figsize=(12, 3))
     figure.suptitle(dataset_name + " by Neural Hawkes")
     ax[0].plot(original_time, label="actual")
@@ -98,7 +121,7 @@ def test_prediction(test_data, idx_test_series, n_samples, start_test_idx, path_
     ax[2].annotate(str(round(rmse, 3)), xy=[1, rmse])
     plt.subplots_adjust(top=0.45)
     figure.tight_layout()
-    plt.savefig(path_to_save + f"{dataset_name}_result.png")
+    plt.savefig(path_to_save + f"{model_name}_result.png")
     plt.show()
     original_type = type_test[start_test_idx:max_len]
 
@@ -133,7 +156,8 @@ def test_prediction(test_data, idx_test_series, n_samples, start_test_idx, path_
     plt.legend()
     plt.xlabel('Time durations')
     plt.ylabel('Event occurance')
-    plt.savefig(path_to_save + f"{dataset_name}_result_prediction.png")
+    plt.savefig(path_to_save + f"{model_name}_result_prediction.png")
     plt.show()
+    acc = accuracy_score(original_type, estimated_types)
 
-    return original_time, original_type, estimated_times, estimated_types, estimated_intensities
+    return original_time, original_type, estimated_times, estimated_types, estimated_intensities, rmse, acc
